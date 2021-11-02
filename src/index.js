f8ecd4701ff1a9561a04574fb2a90d19a823a21b
glnkvn <golnarkavianii@gmail.com>
2021-11-02 14:20:19 +0000

    Add header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const App = () => <h1>Hello World!</h1>
+// const App = () => <h1>Hello World!</h1>
 ReactDOM.render(<App />, document.getElementById('root'));
